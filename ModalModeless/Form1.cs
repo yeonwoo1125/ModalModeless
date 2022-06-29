e43e40d363f0f078aecffccc18a1393606d04730
s2021 <s2021@e-mirim.hs.kr>
2022-06-29 09:38:20 +0900

    #1 메시지 박스, 모달, 모달창, MDI 폼 작성

--- ModalModeless/Form1.cs
+++ ModalModeless/Form1.cs
@@ -15,6 +15,40 @@ namespace ModalModeless
         public Form1()
         {
             InitializeComponent();
+            IsMdiContainer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요", "안녕");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요", "안녕", MessageBoxButtons.YesNoCancel);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요", "안녕", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            frm.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            frm.MdiParent = this;
+            frm.Show();
         }
     }
 }
